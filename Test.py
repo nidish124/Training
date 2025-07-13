c8a077d1d55b75cc8255b9815e46ae32e12695f5
nidish124 <nidish124@gmail.com>
2025-07-13 21:47:54 +0100

    new repo

---
+++ Test.py
@@ -0,0 +1,24 @@
+from flask import request,Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><H1>Welcome to the flask course</H1></html>"
+
+@app.route("/profile/<yourname>", methods=['GET'])
+def profile(yourname):
+    return render_template('profile.html', name=yourname)
+
+@app.route('/calc',methods=['GET','POST'])              
+def calc():
+    num1 = num2 = result = 0
+    if request.method == 'POST':
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+
+        result = num1 + num2
+    return render_template('calc.html', sum=result, num1=num1, num2=num2)
+
+if __name__ == "__main__":
+    app.run(debug=True)
